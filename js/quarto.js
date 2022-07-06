eac5cb8c7d140a871a06f91d4c40571c49f4a19a
LucasLima-207 <lucasdelimadasilva207@gmail.com>
2022-07-06 18:05:02 -0300

    inclementar javascript

--- js/quarto.js
+++ js/quarto.js
@@ -1,30 +1,38 @@
+var quantyRoom1, quantyRoom2, quantyRoom3;
+quantyRoom1 = 20;
+quantyRoom2 = 20;
+quantyRoom3 = 20;
 
-var titleRoom1;
-var titleRoom2;
-var titleRoom3;
+var disponiveis = 'Quantos Disponíveis:  ';
+var quantyRoom1 = document.getElementById('quanty-room1').textContent = disponiveis + quantyRoom1;
+var quantyRoom2 = document.getElementById('quanty-room-2').textContent = disponiveis + quantyRoom2;
+var quantyRoom3 = document.getElementById('quanty-room-3').textContent = disponiveis + quantyRoom3;
+/*
+infomation of rooms
+* */
+var titleRoom1, titleRoom2, titleRoom3;
 
 titleRoom1 = "Quarto Padrão";
 titleRoom2 = "Quarto Vip";
 titleRoom3 = "Quarto Premium";
 
-var el = document.getElementById('title-room-1');
-el.textContent = titleRoom1;
-
-var el2 = document.getElementById('title-room-2');
-el2.textContent = titleRoom2;
-
-var el3 = document.getElementById('title-room-3');
-el3.textContent = titleRoom3;
-
+var titleRoom1 = document.getElementById('title-room-1').textContent = titleRoom1;
+var titleRoom2 = document.getElementById('title-room-2').textContent = titleRoom2;
+var titleRoom3 = document.getElementById('title-room-3').textContent = titleRoom3;
+/* 
+*This area is reponsabilited for mostre the information of room;
+*/
+var inforRoom1, inforRoom2, inforRoom3;
 var priceRoom1 = 100;
 var priceRoom2 = 200;
 var priceRoom3 = 300;
 
-var priceroom1 = document.getElementById('cost-room-1');
-priceroom1.textContent = 'R$ ' +priceRoom1;
- 
-var priceroom2 = document.getElementById('cost-room-2');
-priceroom2.textContent = 'R$ ' + priceRoom2;
-
-var priceroom3 = document.getElementById('cost-room-3');
-priceroom3.textContent = 'R$ ' + priceRoom3;
+inforRoom1 = 'O quarto padrão e equipado com, tv 30polegas, wi-fi, streams, frigobar';
+inforRoom2 = 'O quarto vip possuir, tudo de direito ao usuario comum, mas um bônus de poder te serviço de spar incluído no pacote';
+inforRoom3 = 'O quarto premium é uma experiencia completa para nossos usuarios, possuindo todos  os serviços de quarto disponíveis!'
+var priceRoom1 = document.getElementById('cost-room-1').textContent = 'R$ ' +priceRoom1; 
+var priceRoom2 = document.getElementById('cost-room-2').textContent = 'R$ ' + priceRoom2;
+var priceRoom3 = document.getElementById('cost-room-3').textContent = 'R$ ' + priceRoom3;
+var inforroom1 = document.getElementById('infor-room1').textContent = inforRoom1;
+var inforRoom2 = document.getElementById('infor-room2').textContent = inforRoom2;
+var inforRoom3 = document.getElementById('infor-room3').textContent = inforRoom3;
